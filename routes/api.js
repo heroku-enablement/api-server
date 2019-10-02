59598ca3f99069a10a49285b0fc284e4660cfe10
sho kisaragi <sho@oshiire.to>
2019-10-02 16:37:23 +0900

    change variable

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
   const count = +req.query.count || 10;
 
   [...Array(count)].map((_, i) => {
-    res.write(JSON.stringify({ id: uuidv4(), product: `product_${i + 1}` }))
+    res.write(JSON.stringify({ id: uuidv4(), name: `product_${i + 1}` }))
   });
   res.end();
 });
@@ -16,14 +16,14 @@ router.get('/', (req, res) => {
 // GET /api/v1/products/:id
 router.get('/:id', (req, res) => {
   const id = req.params.id;
-  res.json({ id: uuidv4(), product: `product_${id}` });
+  res.json({ id: uuidv4(), name: `product_${id}` });
 });
 
 // POST /api/v1/products/
 router.post('/', (req, res) => {
   const id = uuidv4();
-  const product = req.params.name;
-  res.json({ id: id, product: product });
+  const name = req.params.name;
+  res.json({ id: id, name: name });
 })
 
 module.exports = router;
